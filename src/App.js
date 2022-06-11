8fe754f40acb6cf1ad5f963130c7649c1de71950
Nursultan34 <nursultanerikov7@gmail.com>
2022-06-11 13:31:02 +0600

    Maps

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import { NavMenu } from './components/navmenu/NavMenu'
 import Catalog from "./components/Catalog/Catalog";
 import { Whiteblock } from "./components/whiteblock/Whiteblock";
 import { Deliveryblock } from "./components/delivery/Deliveryblock";
+import {Routes, Route} from 'react-router-dom'
+import Maps from './components/Maps/Maps';
 
 
 
 
 function App() {
   return (
-   
-
     <div className="container">
+    
       <NavMenu/>
       <Mainblack />
       <Whiteblock/>
       <Catalog />
       <Deliveryblock/>
+      <Maps/>
     </div>
 
 
---
+++ src/components/Maps/Maps.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Maps.css'
+
+function Maps() {
+  return <iframe className='Maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d182.82396200795574!2d74.62282131775626!3d42.847860677242956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7350756bf27%3A0xb71b7bc5fd8f1643!2sAtlasmak!5e0!3m2!1sen!2skg!4v1654931362801!5m2!1sen!2skg" />
+}
+
+export default Maps
