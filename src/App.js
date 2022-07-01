84b6ee573e1a24593a17e37eb82e114266a9db14
Akniet Syrtbaeva <syrtbaevaakniet25@gmail.com>
2022-07-01 11:10:02 +0600

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import react, { useEffect } from "react";
+import React from 'react';
 import './App.css';
 import { Mainblack } from './components/mainblack/Mainblack';
 import { NavMenu } from './components/navmenu/NavMenu'
@@ -8,11 +7,11 @@ import { Whiteblock } from "./components/whiteblock/Whiteblock";
 import { Deliveryblock } from "./components/delivery/Deliveryblock";
 import {Routes, Route} from 'react-router-dom'
 import Maps from './components/Maps/Maps';
-<<<<<<< HEAD
 import CardSecond from "./components/Catalog/Card/CardSecond";
-=======
 import Headfoot from "./components/Headfoot/Headfoot";
->>>>>>> 4c8b4389a9568a367a7f1d1ff2e0740570d4d8ec
+
+
+
 
 
 
@@ -21,17 +20,18 @@ function App() {
   return (
     <div className="container">
     
+      <Headfoot/>
       <NavMenu/>
       <Mainblack />
       <Whiteblock/>
       <CardSecond/>
+      <Catalog/>
       <Deliveryblock/>
-<<<<<<< HEAD
-      {/* <Maps/> */}
-=======
       <Maps/>
       <Headfoot/>
->>>>>>> 4c8b4389a9568a367a7f1d1ff2e0740570d4d8ec
+      
+      
+
     </div>
 
 
